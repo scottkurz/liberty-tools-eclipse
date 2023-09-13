6b868f737d1ff7eca3b4151da5d4e5b0990e1010
Scott Kurz <skurz@us.ibm.com>
2023-09-13 07:03:04 -0700

    re-add setup validation
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- tests/src/main/java/io/openliberty/tools/eclipse/test/it/utils/SWTBotPluginOperations.java
+++ tests/src/main/java/io/openliberty/tools/eclipse/test/it/utils/SWTBotPluginOperations.java
@@ -551,7 +551,6 @@ public class SWTBotPluginOperations {
 
 //        Object parmLabel = find("Start parameters:", parentTreeStatic, Option.factory().widgetClass(Label.class).setExtraLogging(true).build());
 //        Control parmText = ControlFinder.findControlInRange(parmLabel, Text.class, Direction.EAST);
-new SWTWorkbenchBot().captureScreenshot("sksk-screenshot.png");
 
         Object parmText = find("Example: -DhotTests=true", parentTreeStatic, Option.factory().widgetClass(Text.class).setExtraLogging(true).build());
         set(parmText, customParms);
